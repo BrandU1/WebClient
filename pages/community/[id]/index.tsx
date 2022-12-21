6462217fb33181a1fdfa91a045feb6e658aa406a
dabeen <ekqls09@hanyang.ac.kr>
2022-12-21 15:36:03 +0900

    커뮤니티 페이지

--- pages/community/[id]/index.tsx
+++ pages/community/[id]/index.tsx
@@ -1,9 +1,15 @@
 import Post from "@components/pages/community/post";
+import SidePost from "@components/pages/community/sidepost";
+import Recommend from "@components/pages/community/recommend";
 
 function PostPage() {
   return (
-    <div>
-      <Post />
+    <div className="flex flex-row mt-5">
+      <div className="flex flex-col">
+        <Post />
+        <Recommend />
+      </div>
+      <SidePost />
     </div>
   );
 }
