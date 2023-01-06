360bec93436b3b5f4f77cfb9b681bdbe13b57bce
dabeen <ekqls09@hanyang.ac.kr>
2023-01-06 18:04:48 +0900

    클릭 검색 기능

--- lib/fetches.ts
+++ lib/fetches.ts
@@ -1,10 +1,5 @@
 import client from "@lib/api";
 
-// export async function getAddresses() {
-//   const response = await client.get("accounts/addresses");
-//   return response.data;
-// }
-
 export async function getAddress() {
   const response = await client.get("accounts/addresses");
   return response.data;
