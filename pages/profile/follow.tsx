3717b556e0a9999733a633f93ff4e927f1cae390
Jaehyeon99 <janhyun99@naver.com>
2023-01-19 10:12:21 +0900

    팔로우 accounts 수정

--- pages/profile/follow.tsx
+++ pages/profile/follow.tsx
@@ -1,16 +1,29 @@
 import FollowComp from "@components/pages/profile/followcomp";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import client from "@lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { BranduBaseResponse, FollowList } from "../../types/privacy";
 
 function FollowPage() {
   const router = useRouter();
+  const id = router.query.id;
+
+  const getFollow = () => {
+    return client.get(`accounts/follows/${id}`).then((res) => res.data);
+  };
+  const { data, isLoading } = useQuery<BranduBaseResponse<FollowList>>(
+    ["follow"],
+    getFollow
+  );
+
   return (
     <>
       <Head>
         <title>팔로워</title>
       </Head>
       <div>
-        <FollowComp tab={+router.query.tab!} />
+        <FollowComp data={data?.results!} tab={+router.query.tab!} />
       </div>
     </>
   );
